58e21211c921786d87b1013c17e96fa87e179315
Ujjawal Rana <53923101+ujjawalrana@users.noreply.github.com>
2021-09-25 01:25:14 +0530

    Create Edit Distance.cpp

---
+++ CSES/Dynamic Programming/Edit Distance.cpp
@@ -0,0 +1,100 @@
+// https://cses.fi/problemset/task/1639
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll                      long long int
+#define lu                      unsigned long long int
+#define ld                      long double
+#define pb                      push_back
+#define ppb                     pop_back
+#define pf                      push_front
+#define in                      insert
+#define br                      cout<<"\n";
+#define size(x)                 x.size()
+#define all(x)                  x.begin(), x.end() 
+#define mem(x,s)                memset(x,s,sizeof(x))
+#define ps(y,x)                 fixed<<setprecision(y)<<x;
+#define lower(x,a)              lower_bound(x.begin(),x.end(),a)-x.begin()
+#define upper(x,a)              upper_bound(x.begin(),x.end(),a)-x.begin()
+#define find(x,a)               find(x.begin(),x.end(),a)-x.begin()
+#define fast                    ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define umap                    unordered_map
+#define zerobits(x)             __builtin_ctzll(x)
+#define setbits(x)              __builtin_popcountll(x)
+#define ff                      first
+#define ss                      second
+#define google(i)               cout<<"Case #"<<i<<": ";
+ 
+ 
+template<typename T>
+using pbds=tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+ 
+typedef priority_queue<ll,vector<ll>,greater<ll> > min_pq;
+typedef priority_queue<ll> max_pq;
+ 
+ll add(ll a,ll b,ll m){a=a%m;b=b%m;return (((a+b)%m)+m)%m;}
+ll sub(ll a,ll b,ll m){a=a%m;b=b%m;return (((a-b)%m)+m)%m;}
+ll mul(ll a,ll b,ll m){a=a%m;b=b%m;return (((a*b)%m)+m)%m;}
+ 
+const long long int mod=1000000007;
+const long long int inf=1000000000;
+const long double pie=3.14159265358979323846264338;
+ 
+string a,b;
+ 
+void solve(){   
+ 
+    cin>>a>>b;
+ 
+    ll n,m;
+    n=size(a);
+    m=size(b);
+ 
+ 
+    vector<vector<ll>>dp(n+1,vector<ll>(m+1,inf));
+    
+    dp[0][0]=0;
+ 
+    for(ll i=0;i<=n;i++){
+        for(ll j=0;j<=m;j++){
+ 
+            if(i){
+                dp[i][j]=min(dp[i][j],dp[i-1][j]+1);
+            }
+            if(j){
+                dp[i][j]=min(dp[i][j],dp[i][j-1]+1);
+            }
+            if( i and j ){
+                dp[i][j]=min(dp[i][j],dp[i-1][j-1] + (a[i-1]!=b[j-1]) );
+            }
+            
+        }
+    }
+    
+    cout<<dp[n][m];
+    
+}
+ 
+int main(){
+    fast;
+//#ifndef ONLINE_JUDGE
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//#endif
+    #ifdef SIEVE
+        sieve();
+    #endif
+    #ifdef NCR
+        init();
+    #endif
+    ll q=1;
+    // cin>>q;
+    while(q--){
+        solve();br;
+    }    
+}
